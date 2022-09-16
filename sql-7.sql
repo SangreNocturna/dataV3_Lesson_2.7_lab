ead0c5483d469cce21dfe734ca25b311e91dea70
SangreNocturna <jtafernandez@gmail.com>
2022-09-16 17:08:36 +0200

    sql-7.sql

--- sql-7.sql
+++ sql-7.sql
@@ -1,11 +1,18 @@
 -- 1. How many films are there for each of the categories in the category table. Use appropriate join to write this query
 select * from sakila.film;
+select * from sakila.film_category;
 select distinct(count(title)) from sakila.film;
 
-SELECT c.name, c.category_id, f.title as amount 
-FROM sakila.film f
-JOIN sakila.category c
+SELECT c.name, count(f.film_id) as amount
+FROM sakila.category c
+JOIN sakila.film_category f
+ON c.category_id = f.category_id
+GROUP BY c.name;
+-- ORDER BY
 
-limit 15;
-
--- STILL IN PROCESS, IT IS TOO LATE ALREADY
+-- 2. Display the total amount rung up by each staff member in August of 2005
+SELECT s.first_name, s.last_name, count(p.amount)
+FROM sakila.staff s
+JOIN sakila.payment p
+ON s.staff_id = p.staff_id
+GROUP BY p.staff_id;
