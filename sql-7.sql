8c0bba8b7d5f3d2e37168630cf61b3b796b5c6bd
SangreNocturna <jtafernandez@gmail.com>
2022-09-16 17:24:47 +0200

    sql-7.sql

--- sql-7.sql
+++ sql-7.sql
@@ -11,8 +11,32 @@ GROUP BY c.name;
 -- ORDER BY
 
 -- 2. Display the total amount rung up by each staff member in August of 2005
-SELECT s.first_name, s.last_name, count(p.amount)
+SELECT s.staff_id, s.first_name, s.last_name, count(p.amount)
 FROM sakila.staff s
 JOIN sakila.payment p
 ON s.staff_id = p.staff_id
 GROUP BY p.staff_id;
+
+-- 3. Which actor has appeared in the most films?
+SELECT a.first_name, a.last_name, count(film_id)
+FROM sakila.actor a
+JOIN sakila.film_actor f
+ON a.actor_id = f.actor_id
+GROUP BY a.first_name;
+
+-- 4. Most active customer (the customer that has rented the most number of films)
+SELECT c.first_name, c.last_name, count(r.rental_id)
+FROM sakila.customer c
+JOIN sakila.rental r
+ON c.customer_id = r.customer_id
+GROUP BY c.first_name
+ORDER BY count(r.rental_id) DESC;
+
+-- 5. Display the first and last names, as well as the address, of each staff member
+
+
+
+
+ 
+
+
